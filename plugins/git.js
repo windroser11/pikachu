8d2f1b23d936fc357e785d8a8bcc394f1fb652dc
windroser11 <95699984+windroser11@users.noreply.github.com>
2021-12-09 10:45:11 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -10,19 +10,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
     var skl = await axios.get("https://images.wallpapersden.com/image/download/pokemon-detective-pikachu-4k_a2lrZmWUmZqaraWkpJRsZ2WtbGdl.jpg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Pikachu bot created by Ameer Suhail*
-*Creator number : wa.me/919946432377?text=Hi%20Ameer%20Suhail.%20*
-
-*Bot setting video : https://youtu.be/_D4ZYuUSXjs*
-
-*Githublink (Setup)  :    https://github.com/ameer-kallumthodi/pikachu*
-
-*Audio commads :   https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
-
-*Sticker commads : https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
-
-*Video For Setting Bot : https://youtu.be/_D4ZYuUSXjs* 
-
-*For More Updates Subscribe The Channel ☝*
+* Thalkalam theran thalparyam illa venagil ivanodu chodikkku http://wa.me/918606413490?text=Hi%20Windro%20Ser.%20*
 `}) 
 
 }));
